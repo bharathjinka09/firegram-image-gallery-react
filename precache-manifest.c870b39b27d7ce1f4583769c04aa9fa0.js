b30048f6218d0ea73263a8213fce5de148e98773
bharathjinka09 <bharathjinka09@gmail.com>
2020-07-30 17:50:22 +0530

    Updates

--- precache-manifest.c870b39b27d7ce1f4583769c04aa9fa0.js
+++ precache-manifest.c870b39b27d7ce1f4583769c04aa9fa0.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "82fe8529b3e4c79af72b9d8237379abf",
+    "revision": "6bb969099547f8abedbc45df4499bdef",
     "url": "/firegram-image-gallery-react/index.html"
   },
   {
-    "revision": "10e9d6a877b52d1cf677",
+    "revision": "f92ece3aa1237e08c308",
     "url": "/firegram-image-gallery-react/static/css/main.ac4a3041.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/firegram-image-gallery-react/static/js/2.496294bf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "10e9d6a877b52d1cf677",
-    "url": "/firegram-image-gallery-react/static/js/main.3ff9bfb7.chunk.js"
+    "revision": "f92ece3aa1237e08c308",
+    "url": "/firegram-image-gallery-react/static/js/main.195afcf5.chunk.js"
   },
   {
     "revision": "39d2577de7ee3e0779a5",
