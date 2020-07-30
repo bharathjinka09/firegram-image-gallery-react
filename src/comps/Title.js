3b1b98107266801e39e252a4a7d6b47be7ab98ed
bharathjinka09 <bharathjinka09@gmail.com>
2020-07-30 17:49:05 +0530

    added package

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -5,7 +5,6 @@ const Title = () => {
     <div className="title">
       <h1>FireGram</h1>
       <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
   )
 }
